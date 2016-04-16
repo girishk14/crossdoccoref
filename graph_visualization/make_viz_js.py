b649c67462081efa00836f0b89e847c750417102
vijaya rani singh <VijayaSingh@vijayas-MacBook-Pro.local>
2016-04-15 19:07:08 -0500

    Fixing cluster extraction

--- graph_visualization/make_viz_js.py
+++ graph_visualization/make_viz_js.py
@@ -2,53 +2,59 @@ import os
 import sys
 import json
 
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 
 
 def read_cluster_json():
 	cluter_json = None
-	with open('temp2.json','r') as f:
-		cluster_json = json.loads(f.read())
-	count = len(cluster_json.keys())
-	print count
-	with open('temp6.json','r') as f:
-		cluster_json1 =json.loads(f.read())
-	print cluster_json1.keys()
-	for items in cluster_json1:
-		cluster_json[str(count+1)]=cluster_json1[items]
-		count+=1
-
-
-	print cluster_json.keys()
+	with open('trial.json','r') as f:
+		cluster_json =json.loads(f.read())
 	
-
 	graph = {}
 	graph['nodes']  =  []
 	graph['edges'] = []
-
-
 	node_dictionary = {}
 
-
 	entity_counter =  0
+
 	for cluster_member in cluster_json:
 		for subj in cluster_json[cluster_member].keys():
-			
+
+
 			if subj in node_dictionary and node_dictionary[subj]['group']!='Common' and  node_dictionary[subj]['group'] != cluster_member:
 				node_dictionary[subj]['group'] = 'Common'
+				node_dictionary[subj]['members'].append(cluster_member)
 			else:
 				node_dictionary[subj] = {}
 				node_dictionary[subj]['id'] = entity_counter
 				entity_counter+=1
 				node_dictionary[subj]['group'] = cluster_member
+				node_dictionary[subj]['members'] = []
+				node_dictionary[subj]['members'].append(cluster_member)
+
+
 
 				for (rel, obj) in cluster_json[cluster_member][subj]:
 					if obj in node_dictionary and node_dictionary[obj]['group']!='Common' and  node_dictionary[obj]['group'] != cluster_member:
 						node_dictionary[obj]['group'] = 'Common'
+						node_dictionary[obj]['members'].append(cluster_member)
 					else:
 						node_dictionary[obj] = {}
 						node_dictionary[obj]['id'] = entity_counter
+
+						if entity_counter == 18 :
+							for node in node_dictionary:
+								print node
+							sys.exit()
 						entity_counter+=1	
 						node_dictionary[obj]['group'] = cluster_member
+						node_dictionary[obj]['members'] = []
+						node_dictionary[obj]['members'].append(cluster_member)
+
+
+
 
 					edge_obj = {}
 					edge_obj['from'] = node_dictionary[subj]['id']
@@ -61,12 +67,11 @@ def read_cluster_json():
 		node_obj = {}
 		node_obj['id'] = node_dictionary[node]['id']
 		node_obj['group'] = node_dictionary[node]['group']
+		
 		node_obj['label'] = node
+		node_obj['members'] = node_dictionary[node]['members']
+		print(node_obj)
 		graph['nodes'].append(node_obj)
-
-
-
-
 
 		with open('visjson.json', 'w') as f:
 			f.write(json.dumps(graph))
